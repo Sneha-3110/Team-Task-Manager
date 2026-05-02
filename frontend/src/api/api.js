d6f4e0e45f9b8ccdc7c5bb7184c7231482cd65cb
Sneha-3110 <snehaprajapati3110@gmail.com>
2026-05-02 10:39:25 +0530

    Added live backend URL

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+const API = axios.create({ baseURL: 'https://team-task-manager-production-afb8.up.railway.app/api' });
 
 API.interceptors.request.use((req) => {
     const token = localStorage.getItem('token');
